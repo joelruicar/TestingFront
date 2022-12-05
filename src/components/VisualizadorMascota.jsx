a3d3c242a64449d70923369d31b943946c1cf8a8
Joel L. Ruíz Carrillo <luruicar@upv.edu.es>
2022-12-05 19:17:53 +0100

    mas o menos

--- src/components/VisualizadorMascota.jsx
+++ src/components/VisualizadorMascota.jsx
@@ -1,14 +1,30 @@
 import "./VisualizadorMascota.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getPetByID } from "../services/mascotas";
 import DatosMascota from "./DatosMascota";
 import CarouselMascota from "../Carousel/CarouselMascota/CarouselMascota";
 
 const VisualizadorMascota = () => {
   let datosPet = useParams();
   const [pet, setPet] = useState({});
+
   let { id } = useParams();
-  useEffect(() => {}, []);
+
+  useEffect(() => {
+    getPetByID(datosPet.id, datosPet.found)
+      .then(async (response) => {
+        const data = await response.json();
+        if (!response.ok) {
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+        setPet(data);
+      })
+      .catch((error) => {
+        console.error("Hay un error", error);
+      });
+  }, []);
 
   return (
     <>
